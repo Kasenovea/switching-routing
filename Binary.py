22af6cfd100e9cc5ff934a5f5093c6cc5276084c
Erbol Kasenov <44158321+Kasenovea@users.noreply.github.com>
2020-03-26 13:38:43 +0100

    Test version for hierarchical tree

--- Binary.py
+++ Binary.py
@@ -1,6 +1,19 @@
-#!/usr/bin/python
+
 from collections import deque
 
+
+def fromBinarytoIP(string):
+	splitter = 8
+	divided = [string[i:i+splitter] for i in range(0, len(string), splitter)]
+	decimal = []
+	i = 0
+	while i < 4:
+		decimal.append(int(divided[i], 2))
+		i = i + 1
+	IPaddress = str(decimal[0])
+	for i in range(1,4):
+		IPaddress = IPaddress +'.'+ str(decimal[i])
+	return str(IPaddress)
 
 
 def fromIPtoBinary(string):
@@ -14,6 +27,7 @@ def fromIPtoBinary(string):
 		i=i+1
 	return str(addressIP)
 
+
 def paddingAddress(list):
 	i = 0
 	padded_list = list;
@@ -25,6 +39,14 @@ def paddingAddress(list):
 	return padded_list
 
 
+
+
+
+
+
+#####################################################################################################################
+
+
 class Node():
 
 	#initialization of a node for the tree
@@ -33,15 +55,16 @@ class Node():
 		self.left = None
 		self.right = None
 		self.parent = None
-		self.gateway = None
-	
-	#adding a gateway address to the tree
-	def add_action(self, gateway):
-		self.gateway = gateway;
+		self.rule = None
+		
+	#adding a rule address to the tree
+	def add_rule(self, rule):
+		self.rule = rule;
 	
   
 class Tree():
         
+        #initialization of the tree setting the root to None
         def __init__(self):
 		self.root = None
 	
@@ -77,8 +100,13 @@ class Tree():
 				else:
 					#adding nodes to the right one 
 					return self.add_node(key,node = node.right)
-		
-	def add_action(self,key, l, gateway, node):
+	
+
+	
+
+############################################################################################################################
+	#searching a specific node to assign him a rulle		
+	def add_rule(self,key, l, rule, node):
 		if node is None:
 			node = self.root
 
@@ -86,113 +114,282 @@ class Tree():
 			print "key is at the root"
 			return self.root
 		else:
+			#### Never put rule a*, 0, 1 ####
 			
 			if len(node.key) == len(key):
-				print "Action added to node: ", node.key
-				node.add_action(gateway)
+				#print "\nact", rule
+				#print "added to node: ", node.key
+				node.add_rule(rule)
 				l = 0
 				return 
 			elif key[l] == "0" and node.left is not None:
 				l = l + 1
-				return self.add_action(key, l, gateway, node = node.left)
+				return self.add_rule(key, l, rule, node = node.left)
 			
 			elif key[l] == "1" and node.right is not None:
 				l = l + 1
-				return self.add_action(key, l, gateway, node = node.right)
+				return self.add_rule(key, l, rule, node = node.right)
 			else:
 				l = 0;
 				return None
-	
+	###################################################################################################################
 	#print of the tree with nodes ordered by level	
 	def print_tree(self, head, queue=deque()):
 		if head is None:
        			return
-    		print "\nkey: ", head.key, "\nGw: ", head.gateway
+    		print "\nkey: ", head.key, "\nrule: ", head.rule
 		if head.right is not None:
-			print "Node dx: ", head.right.key
-		else:	print "Node dx:  --"
+			print "Node right: ", head.right.key
+		else:	print "Node right:  --"
 		if head.left is not None:
-			print "Node sx: ", head.left.key
-		else:	print "Node sx:  --"
+			print "Node left: ", head.left.key
+		else:	print "Node left:  --"
     		[queue.append(node) for node in [head.left, head.right] if node]
     		if queue:
         		self.print_tree(queue.popleft(), queue)
 			
+	
+###########################################################################################################################
+
+
 	def finding_prefix(self, IP_add_str, n1, i):
 		global last_prefix
-		#IP_add_bin = fromIPtoBinary(IP_add_str);
-		IP_add_bin = IP_add_str;		
-	
+		global data
+		IP_add_bin = fromIPtoBinary(IP_add_str);
+		#IP_add_bin = IP_add_str;		
+		
+		##############################
+		
+
+		data=[]
+		data.append(n1.rule)
+
+		#data.append(last_prefix)
+
+
+		#print "ddd", data
+			###################################################
+
 		if last_prefix == '*':
 			#default address
 			return "*";
-		
-		
+			
+		# search index < of binary address length
 		if i<len(IP_add_bin):
 			
-			
+			data.append(last_prefix)
+			#print "banch of rules at each level", data
+			# next character of the IP is a zero and current node has a child
 			if IP_add_bin[i] == "0" and n1.left is not None:
 				i = i +1;
-				if n1.gateway is not None:
-					last_prefix = n1.gateway;
+
+				if n1.rule is not None:
+					data.append(last_prefix)
+					last_prefix= n1.rule;
+					#print "sss", last_prefix
 				return self.finding_prefix(IP_add_str, n1.left, i);
-	
+
+			# next character of the IP is a one and current node has a child
+
 			elif IP_add_bin[i] == "1" and n1.right is not None:
 				i = i +1;
-				if n1.gateway is not None:
-					last_prefix = n1.gateway;
-				return self.finding_prefix(IP_add_str, n1.right, i);
 				
+
+				if n1.rule is not None:
+					last_prefix = n1.rule;
+					#print "ddd", last_prefix	
+				return self.finding_prefix(IP_add_str, n1.right, i);
+			
+
+			# if I get here, I don't have kids, I'm at the bottom of the tree
 			else:
-				if n1.gateway is not None:
-					print "search finished\n"
-					return n1.gateway;
+
+				if n1.rule is not None:
+					
+					print "searching stopped we are at rule"
+					return n1.rule;
+
 				else:
 					return last_prefix;
 		else:
 			
-			print "no matchi", last_prefix
+
+			## no final matching, return to saved prefix
+			print "\nSearching stopped: we found nothing, last prefix is : ", last_prefix
 			return last_prefix;
-	
+			
+###############################################################################################################
 
-
-		
-k="192.123.34.34"			
-sd=fromIPtoBinary(k)
-print"/n Ip address translated to binary version",k		
-print sd		
 length=0
 last_prefix=None 
-t=Tree()
-t.add_node(sd)
-t.add_node("0")
-t.add_node("1")
-t.add_node("01")
-t.add_node("00")
-t.add_node("001")
-t.add_node("0010")
-t.add_node("0011")
-t.add_node("00110")
-t.add_node("001101")
-t.add_node("10")
-t.add_node("11")
-t.add_node("100")
-t.add_node("101")
-t.add_node("1010")
-t.add_node("1011")
-t.add_node("10110")
-t.add_node("101100")
-t.add_node("101101")
-
-t.add_action("101", 0, "sdsd", None);
-t.add_action("01", 0, "ssss", None);
-t.add_action("0010", 0, "128.3", None);
-t.add_action("001101", 0, "128.1", None);
-t.add_action("11000000011110110010001000100010", 0, "action drop packet", None);
+f1=Tree()
+f1.add_node("*")
+f1.add_node("0")
+f1.add_node("1")
+f1.add_node("00")
+f1.add_node("000")
+f1.add_node("0000")
+f1.add_node("001")
+f1.add_node("0010")
+f1.add_node("01")
+f1.add_node("011")
+f1.add_node("0110")
+f1.add_node("11")
+f1.add_node("111")
+f1.add_node("1111")
+f1.add_node("1110")
+f1.add_node("110")
+f1.add_node("1100")
+f1.add_node("10")
+f1.add_node("101")
+f1.add_node("1010")
 
 
-x=t.finding_prefix("110000000111101100100010001",t.root,0)
-print x
 
 
-t.print_tree(t.root)
+#t.add_node("0010")
+#t.add_node("0011")
+#t.add_node("00110")
+#t.add_node("001101")
+#t.add_node("10")
+#t.add_node("11")
+#t.add_node("100")
+#t.add_node("101")
+#t.add_node("1010")
+#t.add_node("1011")
+#t.add_node("10110")
+#t.add_node("101100")
+#t.add_node("101101")
+
+
+f1.add_rule("0000", 0, "rule1", None);
+f1.add_rule("0010", 0, "rule2", None);
+f1.add_rule("0110",0,"rule3",None);
+f1.add_rule("011",0,"rule4",None);
+f1.add_rule("1010",0,"rule5",None);
+f1.add_rule("1100",0,"rule6",None);
+f1.add_rule("1110",0,"rule7",None);
+f1.add_rule("1111",0,"rule8",None);
+
+
+#f1.add_rule("0010", 0, "128.30.30.30", None);
+#f1.add_rule("001101", 0, "128.15.0.0", None);
+#f1.add_rule("10110", 0, "64.128.0.0", None);
+t=fromIPtoBinary('121.222.111.22')
+print t
+
+x1=f1.finding_prefix('121.222.111.22',f1.root,0)
+print "prefix in f1",x1
+
+f1=data
+print "all rules in f1", data
+
+f2=Tree()
+f2.add_node("*")
+#f2.add_node_F2("0")
+f2.add_node("1")
+#f2.add_node_F2("00")
+f2.add_node("0")
+f2.add_node("00")
+f2.add_node("000")
+f2.add_node("0000")
+f2.add_node("0001")
+f2.add_node("01")
+f2.add_node("010")
+f2.add_node("0100")
+f2.add_node("0101")
+f2.add_node("011")
+f2.add_node("0110")
+f2.add_node("11")
+f2.add_node("111")
+f2.add_node("1111")
+f2.add_node("10")
+f2.add_node("100")
+f2.add_node("1000")
+f2.add_node("1001")
+
+
+
+
+
+
+#adding rule 
+
+f2.add_rule("1001",0,"rule1",None)
+f2.add_rule("1000",0,"rule2",None)
+f2.add_rule("0101",0,"rule3",None)
+f2.add_rule("0110",0,"rule4",None)
+f2.add_rule("0100",0,"rule5",None)
+f2.add_rule("0001",0,"rule6",None)
+f2.add_rule("0100",0,"rule7",None)
+f2.add_rule("1111",0,"rule8",None)
+
+
+
+t2=fromIPtoBinary('232.111.22.11')
+print t2
+x2=f2.finding_prefix('232.111.22.11',f2.root,0)
+
+
+print "\nprefix in f2",x2
+f2=data
+print "all rules in f2", data
+
+
+
+
+f3=Tree()
+f3.add_node("*")
+f3.add_node("0")
+f3.add_node("1")
+f3.add_node("00")
+f3.add_node("01")
+f3.add_node("10")
+f3.add_node("11")
+f3.add_node("000")
+f3.add_node("001")
+f3.add_node("010")
+f3.add_node("011")
+f3.add_node("111")
+f3.add_node("101")
+
+
+f3.add_rule("010",0,"rule1",None)
+f3.add_rule("111",0,"rule2",None)
+
+f3.add_rule("000",0,"rule3",None)
+f3.add_rule("01",0,"rule4",None)
+f3.add_rule("011",0,"rule5",None)
+f3.add_rule("001",0,"rule1",None)
+f3.add_rule("101",0,"rule1",None)
+f3.add_rule("11",0,"rule1",None)
+
+
+
+
+
+
+t3=fromIPtoBinary('110.220.22.3')
+print t3
+x3=f3.finding_prefix('110.220.22.3',f3.root,0)
+print "\nprefix in f3",x3
+f3=data
+print "all rules in f3", data
+junk=f1+f2+f3
+print "junk", junk
+
+
+if x1==x2:
+	if x2==x3:
+		rule=x3
+		print "stop at f3",rule
+	else:
+		rule=x2
+		print "stop at f2",rule
+
+else:
+	rule=x1
+	print "stop at f1",rule
+
+
+#f4.print_tree(f4.root)
